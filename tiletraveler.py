24e821291b27f3ee947e5eae65eeca5560ba3ad6
Aríel Jóhann Árnason <ariel17@ru.is>
2018-09-18 11:04:04 +0000

    uppfærður kóði

--- tiletraveler.py
+++ tiletraveler.py
@@ -20,43 +20,51 @@ path = ""
 #Rooms fall
 room1_1 = "(N)orth"; room1_2 = "(N)orth, (E)ast, (S)outh"; room1_3 = "(S)outh, (E)ast"
 room2_1 = "(N)orth"; room2_2 = "(W)est, (S)outh"; room2_3 = "(E)ast, (W)est"
-room3_1 = "(N)orth"; room3_2 = "(N)orth, (S)outh"; room3_3 = "(N)orth"
+room3_1 = "(N)orth"; room3_2 = "(N)orth, (S)outh"; room3_3 = "(S)outh"
+#
 
 path = room1_1
+valid_direction = 'n'
 
 while 1:
+    
     print("You can travel: " + path)
     path = ''
     direction = input("Direction: ").lower()
     
 #dir mover(direction):
 
-    if (direction == 'n') and (char_y < 3):
+    if (direction == 'n') and ('n' in valid_direction):
         char_y += 1
-    elif (direction == 's') and (char_y > 1):
+    elif (direction == 's') and ('s' in valid_direction):
         char_y -= 1
-    elif (direction == 'e') and (char_x < 3):
+    elif (direction == 'e') and ('e' in valid_direction):
         char_x += 1
-    elif (direction == 'w') and (char_x > 1):
+    elif (direction == 'w') and ('w' in valid_direction):
         char_x -= 1
     else:
         print("Not a valid direction!")
         # return char_x, char_y
 
+#dir veggir():
 
-    print("Current location",char_x, char_y)
 
-    if char_x == 1 and char_y == 1: path = room1_1
-    elif char_x == 1 and char_y == 2: path = room1_2
-    elif char_x == 1 and char_y == 3: path = room1_3
-    elif char_x == 2 and char_y == 1: path = room2_1
-    elif char_x == 2 and char_y == 2: path = room2_2
-    elif char_x == 2 and char_y == 3: path = room2_3
+    if char_x == 1 and char_y == 1: path = room1_1; valid_direction = 'n'
+    elif char_x == 1 and char_y == 2: path = room1_2; valid_direction = 'nes'
+    elif char_x == 1 and char_y == 3: path = room1_3; valid_direction = 'es'
+    elif char_x == 2 and char_y == 1: path = room2_1; valid_direction = 'ws'
+    elif char_x == 2 and char_y == 2: path = room2_2; valid_direction = 'ws'
+    elif char_x == 2 and char_y == 3: path = room2_3; valid_direction = 'we'
     elif char_x == 3 and char_y == 1: path = room3_1; print("Victory!"); break
-    elif char_x == 3 and char_y == 2: path = room3_2
-    elif char_x == 3 and char_y == 3: path = room3_3
+    elif char_x == 3 and char_y == 2: path = room3_2; valid_direction = 'ns'
+    elif char_x == 3 and char_y == 3: path = room3_3; valid_direction = 'ws'
+    
+    # if 'e' in walls:
+    #     char_x -= 1
+    #     print("Not a valid direction!")
     
 
+    # print("Current location",char_x, char_y)
 
 
 
