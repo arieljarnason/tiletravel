7fe08e4b1b70039e8635aff6c9d7c43664c6df76
Aríel Jóhann Árnason <ariel17@ru.is>
2018-09-18 09:21:42 +0000

    first commit

---
+++ tiletraveler.py
@@ -0,0 +1,11 @@
+""" með tvo int: char_x og char_y sem tilgreinir hvar character er
+    if( x og y) þá hvað: m.v. aðstæður á hverjum stað fyrir sig
+    þegar character nær inn í 3.1 þá victory
+    í hverri loopu skrifast út status update, you can travel: 
+
+    """
+
+#FASTAR
+
+
+char_x, char_y = 1, 1
