4f0944196fdab261f07bcf0553a8eafb8ed460cb
Aríel Jóhann Árnason <ariel17@ru.is>
2018-09-20 09:27:09 +0000

    Síðasta uppfærsla gerð, leysti leiðinlega falla-tengingar-erfiðleika

--- tiletraveler2.py
+++ tiletraveler2.py
@@ -1,15 +1,19 @@
 
 #Upphafsstaða characters
-char_x, char_y = 1, 1
+# char_x, char_y = 1, 1
 #Opna hurðin í fyrsta herbergi er í Norður, 'n'
 
 def print_doorways_available(doors):
     """Prentar út hverjar mögulegar dyragættir eru"""
     print("You can travel:", doors)
 
+def print_victory():
+    """Prentar victory!"""
+    print("Victory!")
+
 def character_locator(char_x, char_y):
     """Finnur staðsetningu characters miðað við upplýsingar úr korti"""
-
+    victory = False
     if char_x == 1 and char_y == 1:   doorways = 'n' 
     elif char_x == 1 and char_y == 2: doorways = 'nes' 
     elif char_x == 1 and char_y == 3: doorways = 'es'  
@@ -18,9 +22,9 @@ def character_locator(char_x, char_y):
     elif char_x == 2 and char_y == 3: doorways = 'we'  
     elif char_x == 3 and char_y == 2: doorways = 'ns'  
     elif char_x == 3 and char_y == 3: doorways = 'ws'
-    elif char_x == 3 and char_y == 1: doorways = '' 
+    elif char_x == 3 and char_y == 1: doorways = ''; victory = True
     
-    return doorways
+    return doorways, victory
 
 def compass(direction):
     """tekur inn doorways kóða úr "character_locator" og sendir út réttan texta"""
@@ -46,34 +50,31 @@ def mover(char_nextmove, opendoors, char_x, char_y):
     charmoves_x = char_x
     charmoves_y = char_y
 
-    if (char_nextmove == 'n') and ('n' in opendoors):
-        charmoves_y += 1; print("Current position: ", charmoves_x, charmoves_y); return charmoves_y
-    elif (char_nextmove == 's') and ('s' in opendoors):
-        charmoves_y -= 1; return charmoves_y
-    elif (char_nextmove == 'e') and ('e' in opendoors):
-        charmoves_x += 1; return charmoves_x
-    elif (char_nextmove == 'w') and ('w' in opendoors):
-        charmoves_x -= 1; return charmoves_x
+    if (char_nextmove == 'n') and ('n' in opendoors): charmoves_y += 1; charmoves_x, charmoves_y
+    elif (char_nextmove == 's') and ('s' in opendoors): charmoves_y -= 1; charmoves_x, charmoves_y
+    elif (char_nextmove == 'e') and ('e' in opendoors): charmoves_x += 1; charmoves_x, charmoves_y
+    elif (char_nextmove == 'w') and ('w' in opendoors): charmoves_x -= 1; charmoves_x, charmoves_y
     else:
         print("Not a valid direction!")
+    return charmoves_x, charmoves_y
 
-
+char_x=1; char_y=1
 
 while 1:
-    
-    char_doorways = character_locator(char_x, char_y)  # character_locator(char_x, char_y) #Character locator finnur hvar character er
+
+
+    char_doorways, victory = character_locator(char_x, char_y)  # character_locator(char_x, char_y) #Character locator finnur hvar character er
     #Býr til char_doorways breytuna úr nsew kóða úr character_locator
-                                    
+    if victory == True:
+        print_victory()
+        break    
+    
     mypath = compass(char_doorways) #Sendir nsew kóða í compass til að finna réttan texta
     #Býr til mypath breytuna úr compass
-    
+    print("mypath:", mypath)
     print_doorways_available(mypath) #Prentar
 
     char_nextmove = set_new_position() #Tekur inn input notanda um átt
-    
-    mover(char_nextmove, char_doorways, char_x, char_y))
+    char_x, char_y = mover(char_nextmove, char_doorways, char_x, char_y)
 
-    
-
-  
 
